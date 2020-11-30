90c0131ad4035606207bb983dcad3de921d6a075
David Brundage <david.brundage@kxvlsoftware.com>
2020-11-30 15:36:26 -0500

    greeting

---
+++ lib/whats_your_name.ex
@@ -0,0 +1,14 @@
+defmodule WhatsYourName do
+  def greet do
+    name = "hello, I am Lary! What's your name?"
+    respond(name)
+  end
+
+  def respond(name) do
+    case(String.trim(name) |> String.capitalize()) do
+      "David" -> "Wow!!! That is my creator's name. I am so glad we have met."
+      "Kaitlyn" -> "I really like that name. You must be very nice."
+      _ -> "It is very nice to meet you, #{name}."
+    end
+  end
+end
